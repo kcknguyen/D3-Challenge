ff2dd085fa62be32bf16cc9232cdb16af024cd08
kcknguyen <71248778+kcknguyen@users.noreply.github.com>
2021-03-02 15:34:37 -0600

    app

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+const svgWidth = 960;
+const svgHeight = 640;
+
+const margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
