71cf1ba92e6d6c1d4cbbc9f3d5574c3a90920dbe
kcknguyen <71248778+kcknguyen@users.noreply.github.com>
2021-03-02 18:19:00 -0600

    commiting

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -21,3 +21,40 @@ var svg = d3
   .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth);
+
+  // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+//d3.csv("hours-of-tv-watched.csv").then(function(tvData) {
+
+// Print the tvData
+//console.log(tvData);
+
+// Cast the hours value to a number for each piece of tvData
+//tvData.forEach(function(data) {
+  //data.hours = +data.hours;
+//});
+
+//var barSpacing = 10; // desired space between each bar
+//var scaleY = 10; // 10x scale on rect height
+
+// Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
+//var barWidth = (chartWidth - (barSpacing * (tvData.length - 1))) / tvData.length;
+
+// @TODO
+// Create code to build the bar chart using the tvData.
+//chartGroup.selectAll(".bar")
+ // .data(tvData)
+  //.enter()
+  //.append("rect")
+  //.classed("bar", true)
+  //.attr("width", d => barWidth)
+  //.attr("height", d => d.hours * scaleY)
+  //.attr("x", (d, i) => i * (barWidth + barSpacing))
+  //.attr("y", d => chartHeight - d.hours * scaleY);
+//}).catch(function(error) {
+//console.log(error);
+//})
